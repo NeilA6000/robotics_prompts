aba9f0a5571a5483814212685bf2d484a096a9f0
NeilA6000 <neilaggarwal11@gmail.com>
2025-02-21 10:06:20 -0700

    feb20.py

---
+++ feb20.py
@@ -0,0 +1,4 @@
+Food = input("What is your favorite food? ")
+Plant = input("Name a type of plant. ")
+Cooking = input("Name a method of cooking. ")  
+print("Today's food will be " + Food + " with " + Plant + " and " + Cooking)
