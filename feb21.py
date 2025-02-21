5273609c08dfd381dceeda4f5dedb6dda8669931
NeilA6000 <neilaggarwal11@gmail.com>
2025-02-21 10:13:09 -0700

    feb21.py

---
+++ feb21.py
@@ -0,0 +1,8 @@
+print ("Welcome to your adventure simulator. Please answer the following questions to get started.")
+Name = input ("What is your name? ")
+Enemy = input ("What is your worst enemy's name? ")
+Superpower = input ("What is your superpower? ")
+Location = input ("Where do you live? ")
+Food = input ("What is your favorite food? ")
+
+print ("Hello", Name, "! Your ability to", Superpower, "will make sure you never have to look at" , Enemy, "again. Go eat", Food, "as you walk down the streets of" , Location, "and use", Superpower, "for good and not evil!")
